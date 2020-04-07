43cfed4069462eaebfd824269527e846fdc7d3f2
João Paulo Taylor Ienczak Zanette <jpaulotiz@gmail.com>
2020-04-07 10:14:21 -0300

    Simple project with internal lib structure. Missing DLL linkage.

---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+#include <mylib.h>
+
+int main(void) {
+    printf("Hello, world!\nFoo = %d\n", foo());
+}
---
+++ src/mylib/mylib.c
@@ -0,0 +1,6 @@
+#include "mylib.h"
+
+int foo(void) {
+    return 5;
+}
+
